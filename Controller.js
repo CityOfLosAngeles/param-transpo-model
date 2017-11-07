f999a2dc06184eb641918c0120e4403f582e23d9
kevinlam825 <kevin.lam825@gmail.com>
2017-11-07 09:27:06 -0800

    Fixed 1a layer issues

--- Controller.js
+++ Controller.js
@@ -32,7 +32,7 @@ require([
     "esri/symbols/SimpleFillSymbol", "esri/symbols/SimpleLineSymbol",
 
     "dojo/i18n!esri/nls/jsapi",
-    "dojo/_base/array", "dojo/parser", "dojo/keys","dojo/dom",
+    "dojo/_base/array", "dojo/parser", "dojo/keys", "dojo/dom",
     "dijit/layout/BorderContainer", "dijit/layout/ContentPane",
     "dojo/domReady!",
 
@@ -51,7 +51,7 @@ require([
 
 
     jsapiBundle,
-    arrayUtils, parser, keys,dom
+    arrayUtils, parser, keys, dom
 ) {
     parser.parse();
     var portalUrl = "https://www.arcgis.com";
@@ -67,141 +67,140 @@ require([
     var score_content = document.getElementById('score');
 
 
-    on(dom.byId("uploadForm"), "change", function (event) {
-                var fileName = event.target.value.toLowerCase();
+    on(dom.byId("uploadForm"), "change", function(event) {
+        var fileName = event.target.value.toLowerCase();
 
-                if (sniff("ie")) { //filename is full path in IE so extract the file name
-                  var arr = fileName.split("\\");
-                  fileName = arr[arr.length - 1];
-                }
-                if (fileName.indexOf(".zip") !== -1) {//is file a zip - if not notify user
-                  generateFeatureCollection(fileName);
-                }
-                else {
-                  dom.byId('upload-status').innerHTML = '<p style="color:red">Add shapefile as .zip file</p>';
-                }
-              });
-
+        if (sniff("ie")) { //filename is full path in IE so extract the file name
+            var arr = fileName.split("\\");
+            fileName = arr[arr.length - 1];
+        }
+        if (fileName.indexOf(".zip") !== -1) { //is file a zip - if not notify user
+            generateFeatureCollection(fileName);
+        } else {
+            dom.byId('upload-status').innerHTML = '<p style="color:red">Add shapefile as .zip file</p>';
+        }
+    });
 
 
-              function generateFeatureCollection (fileName) {
-              var name = fileName.split(".");
-              //Chrome and IE add c:\fakepath to the value - we need to remove it
-              //See this link for more info: http://davidwalsh.name/fakepath
-              name = name[0].replace("c:\\fakepath\\", "");
 
-              dom.byId('upload-status').innerHTML = '<b>Loading… </b>' + name;
+    function generateFeatureCollection(fileName) {
+        var name = fileName.split(".");
+        //Chrome and IE add c:\fakepath to the value - we need to remove it
+        //See this link for more info: http://davidwalsh.name/fakepath
+        name = name[0].replace("c:\\fakepath\\", "");
 
-              //Define the input params for generate see the rest doc for details
-              //http://www.arcgis.com/apidocs/rest/index.html?generate.html
-              var params = {
-                'name': name,
-                'targetSR': map.spatialReference,
-                'maxRecordCount': 1000,
-                'enforceInputFileSizeLimit': true,
-                'enforceOutputJsonSizeLimit': true
-              };
+        dom.byId('upload-status').innerHTML = '<b>Loading… </b>' + name;
 
-              //generalize features for display Here we generalize at 1:40,000 which is approx 10 meters
-              //This should work well when using web mercator.
-              var extent = scaleUtils.getExtentForScale(map, 40000);
-              var resolution = extent.getWidth() / map.width;
-              params.generalize = true;
-              params.maxAllowableOffset = resolution;
-              params.reducePrecision = true;
-              params.numberOfDigitsAfterDecimal = 0;
+        //Define the input params for generate see the rest doc for details
+        //http://www.arcgis.com/apidocs/rest/index.html?generate.html
+        var params = {
+            'name': name,
+            'targetSR': map.spatialReference,
+            'maxRecordCount': 1000,
+            'enforceInputFileSizeLimit': true,
+            'enforceOutputJsonSizeLimit': true
+        };
 
-              var myContent = {
-                'filetype': 'shapefile',
-                'publishParameters': JSON.stringify(params),
-                'f': 'json',
-                'callback.html': 'textarea'
-              };
+        //generalize features for display Here we generalize at 1:40,000 which is approx 10 meters
+        //This should work well when using web mercator.
+        var extent = scaleUtils.getExtentForScale(map, 40000);
+        var resolution = extent.getWidth() / map.width;
+        params.generalize = true;
+        params.maxAllowableOffset = resolution;
+        params.reducePrecision = true;
+        params.numberOfDigitsAfterDecimal = 0;
 
-              //use the rest generate operation to generate a feature collection from the zipped shapefile
-              request({
-                url: portalUrl + '/sharing/rest/content/features/generate',
-                content: myContent,
-                form: dom.byId('uploadForm'),
-                handleAs: 'json',
-                load: lang.hitch(this, function (response) {
-                  if (response.error) {
+        var myContent = {
+            'filetype': 'shapefile',
+            'publishParameters': JSON.stringify(params),
+            'f': 'json',
+            'callback.html': 'textarea'
+        };
+
+        //use the rest generate operation to generate a feature collection from the zipped shapefile
+        request({
+            url: portalUrl + '/sharing/rest/content/features/generate',
+            content: myContent,
+            form: dom.byId('uploadForm'),
+            handleAs: 'json',
+            load: lang.hitch(this, function(response) {
+                if (response.error) {
                     errorHandler(response.error);
                     return;
-                  }
-                  var layerName = response.featureCollection.layers[0].layerDefinition.name;
-                  dom.byId('upload-status').innerHTML = '<b>Loaded: </b>' + layerName;
-                  addShapefileToMap(response.featureCollection);
-                }),
-                error: lang.hitch(this, errorHandler)
-              });
-            }
+                }
+                var layerName = response.featureCollection.layers[0].layerDefinition.name;
+                dom.byId('upload-status').innerHTML = '<b>Loaded: </b>' + layerName;
+                addShapefileToMap(response.featureCollection);
+            }),
+            error: lang.hitch(this, errorHandler)
+        });
+    }
 
 
 
-            function errorHandler (error) {
-            dom.byId('upload-status').innerHTML =
+    function errorHandler(error) {
+        dom.byId('upload-status').innerHTML =
             "<p style='color:red'>" + error.message + "</p>";
-            }
+    }
 
 
 
-            function addShapefileToMap (featureCollection) {
-            //add the shapefile to the map and zoom to the feature collection extent
-            //If you want to persist the feature collection when you reload browser you could store the collection in
-            //local storage by serializing the layer using featureLayer.toJson()  see the 'Feature Collection in Local Storage' sample
-            //for an example of how to work with local storage.
-            var fullExtent;
-            var layers = [];
+    function addShapefileToMap(featureCollection) {
+        //add the shapefile to the map and zoom to the feature collection extent
+        //If you want to persist the feature collection when you reload browser you could store the collection in
+        //local storage by serializing the layer using featureLayer.toJson()  see the 'Feature Collection in Local Storage' sample
+        //for an example of how to work with local storage.
+        var fullExtent;
+        var layers = [];
 
-            arrayUtils.forEach(featureCollection.layers, function (layer) {
-              var infoTemplate = new InfoTemplate("Details", "${*}");
-              var featureLayer = new FeatureLayer(layer, {
+        arrayUtils.forEach(featureCollection.layers, function(layer) {
+            var infoTemplate = new InfoTemplate("Details", "${*}");
+            var featureLayer = new FeatureLayer(layer, {
                 infoTemplate: infoTemplate
-              });
-              //associate the feature with the popup on click to enable highlight and zoom to
-              featureLayer.on('click', function (event) {
-                map.infoWindow.setFeatures([event.graphic]);
-              });
-              //change default symbol if desired. Comment this out and the layer will draw with the default symbology
-              changeRenderer(featureLayer);
-              fullExtent = fullExtent ?
-                fullExtent.union(featureLayer.fullExtent) : featureLayer.fullExtent;
-              layers.push(featureLayer);
             });
-            map.addLayers(layers);
-            map.setExtent(fullExtent.expand(1.25), true);
+            //associate the feature with the popup on click to enable highlight and zoom to
+            featureLayer.on('click', function(event) {
+                map.infoWindow.setFeatures([event.graphic]);
+            });
+            //change default symbol if desired. Comment this out and the layer will draw with the default symbology
+            changeRenderer(featureLayer);
+            fullExtent = fullExtent ?
+                fullExtent.union(featureLayer.fullExtent) : featureLayer.fullExtent;
+            layers.push(featureLayer);
+        });
+        map.addLayers(layers);
+        map.setExtent(fullExtent.expand(1.25), true);
 
-            dom.byId('upload-status').innerHTML = "";
-          }
+        dom.byId('upload-status').innerHTML = "";
+    }
 
 
-          function changeRenderer (layer) {
-            //change the default symbol for the feature collection for polygons and points
-            var symbol = null;
-            switch (layer.geometryType) {
-              case 'esriGeometryPoint':
+    function changeRenderer(layer) {
+        //change the default symbol for the feature collection for polygons and points
+        var symbol = null;
+        switch (layer.geometryType) {
+            case 'esriGeometryPoint':
                 symbol = new PictureMarkerSymbol({
-                  'angle': 0,
-                  'xoffset': 0,
-                  'yoffset': 0,
-                  'type': 'esriPMS',
-                  'url': 'https://static.arcgis.com/images/Symbols/Shapes/BluePin1LargeB.png',
-                  'contentType': 'image/png',
-                  'width': 20,
-                  'height': 20
+                    'angle': 0,
+                    'xoffset': 0,
+                    'yoffset': 0,
+                    'type': 'esriPMS',
+                    'url': 'https://static.arcgis.com/images/Symbols/Shapes/BluePin1LargeB.png',
+                    'contentType': 'image/png',
+                    'width': 20,
+                    'height': 20
                 });
                 break;
-              case 'esriGeometryPolygon':
+            case 'esriGeometryPolygon':
                 symbol = new SimpleFillSymbol(SimpleFillSymbol.STYLE_SOLID,
-                  new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
-                    new Color([112, 112, 112]), 1), new Color([136, 136, 136, 0.25]));
+                    new SimpleLineSymbol(SimpleLineSymbol.STYLE_SOLID,
+                        new Color([112, 112, 112]), 1), new Color([136, 136, 136, 0.25]));
                 break;
-            }
-            if (symbol) {
-              layer.setRenderer(new SimpleRenderer(symbol));
-            }
-          }
+        }
+        if (symbol) {
+            layer.setRenderer(new SimpleRenderer(symbol));
+        }
+    }
 
 
     var map = new Map("map", {
@@ -428,6 +427,7 @@ require([
         transDemand.setRenderer(renderer);
     });
 
+    /*
     transitEN.on("load", function() {
         var renderer = new SimpleRenderer(new SimpleFillSymbol().setColor(new Color([170, 102, 205])).setOutline(new SimpleLineSymbol().setWidth(0.1).setColor(new Color([39, 108, 205]))));
         transitEN.setRenderer(renderer);
@@ -453,7 +453,7 @@ require([
         greenN.setRenderer(renderer);
     });
 
-
+*/
     //Need to recolor these 2 layers -- erase this comment when done
     highInjuryNetworkBuffer.on("load", function() {
         var renderer = new SimpleRenderer(new SimpleFillSymbol().setColor(new Color([0, 128, 0])).setOutline(new SimpleLineSymbol().setWidth(0.1).setColor(new Color([39, 108, 205]))));
@@ -667,26 +667,6 @@ require([
 
             } //end of SelectInBuffer
 
-            /*
-                        function schoolBufferScore(schoolBuffer) {
-                            var score = 0;
-                            if (schoolBuffer.length > 0) {
-                                score = 5;
-                            }
-                            console.log("Half mile school buffer score = " + score);
-                            return score;
-                        }
-
-                        function schoolPolysScore(schoolPolys) {
-                            var score = 0;
-                            if (schoolPolys.length > 0) {
-                                score = 5;
-                            }
-                            console.log("School safety route score = " + score);
-                            score_content.innerHTML += "School safety route score = " + score + "<br>"
-                            return score;
-                        }
-            */
 
             //Start of Section 1 Scoring
             //Need to implement scoring for 1a, 1b, 1d
@@ -824,7 +804,7 @@ require([
 
                 var total = section2TotalScore + section3TotalScore + section4TotalScore + section5TotalScore;
                 var weighted = section2WeightedScore + section3WeightedScore + section4WeightedScore + section5WeightedScore;
-                score_content.innerHTML += "<b>Total Score = " + total + "</b><br>";
+                score_content.innerHTML += "<br><b>Total Score = " + total + "</b><br>";
                 score_content.innerHTML += "<b>Weighted Score = " + weighted + "</b><br>";
 
             }
